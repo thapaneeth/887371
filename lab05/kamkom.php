ffd7b7027b9f86a555e5de4225f7f2b4c30f5b62
thapaneeth <58160167@go.buu.ac.th>
2017-02-23 20:34:34 +0700

    add hello world

---
+++ lab05/kamkom.php
@@ -0,0 +1,31 @@
+<?php
+$a = array(
+    "สิ่งสำคัญที่่สุดคือเราต้องเชื่อว่าเราาทำได้",
+    "ผมรู้...ว่าผมไม่รู้อะไร",
+    "ความสำเร็จมีขั้นตอนอย่าใจร้อน",
+    "อินเทรนด์ สำหรับผมคือเอาท์",
+    "จะสำเร็จไม่จำเป็นต้องเปลี่ยนผู้นำเสมอ",
+    "Less is more",
+    "จงทำซะตอนนี้ บางครั้งคำว่า \"ไว้ทีหลัง\" จะกลายเป็นคำว่า \"ไม่มีวัน\"",
+    "The future depends on what we do in the present.",
+    "Our life is what our thoughts make it. ",
+    "A good plan today is better than a perfect plan tomorrow.",
+    "The only real failure in life is the failure to try.",
+    "Losers make promises they often break. Winners make commitments they always keep. ",
+    "Freedom is the oxygen of the soul.",
+    "Everything has beauty, but not everyone sees it. ",
+    "Forgiveness is a gift you give yourself. ",
+    "When you forgive, you in no way change the past but you sure do change the future.",
+    "Never tell your problems to anyone...20% don't care and the other 80% are glad you have them. ",
+    "วามพ่ายแพ้ไม่ใช่ความล้มเหลวที่แย่ที่สุด แต่การไม่พยายามต่างหาก คือความล้มเหลว ที่แท้จริง",
+    "Behind my smile is everything you’ll never understand.",
+    "เรื่องโหดร้ายที่สุดของการคิดถึงคน ๆ หนึ่ง คือ การได้นั่งอยู่ข้าง ๆ กันทั้งที่รู้ดีว่าทุกอย่างไม่มีวันเหมือนเดิม",
+    "You can close your eyes to things you don’t want to see, but you can’t close your heart to things you don’t want to feel.",
+    "The walls we build around us to keep out the sadness also keep out the joy.",
+    "This year's success was last year's impossibility.",
+    "Who never made a mistake never made a discovery.",
+    "If you don't stand for something, you'll fall for anything.",
+
+);
+$randoom_key = array_rand($a,1);
+echo "<p>" . $a[$randoom_key]."</p>";
